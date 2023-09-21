83da2db880c7fb6afa9e5e9e5459fc7578c47d7b
nomercy360 <maximkadocnikov@gmail.com>
2023-09-21 09:15:47 +0700

    few changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,17 +4,33 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                deepPurple: '#5E58F6',
-                softPink: '#F4EAE9',
-                pastelPurple: '#CDCBFB',
-                mutedPink: '#D5B5C7',
                 background: '#F6F6F6',
+                brown: {
+                    bg: {
+                        DEFAULT: '#F4EAE9',
+                        secondary: '#E2D9D8',
+                    },
+                    DEFAULT: '#B2A3A1',
+                    secondary: '#AB9491',
+                },
+                purple: {
+                    bg: {
+                        DEFAULT: '#CDCBFB',
+                        secondary: '#C1BFFF',
+                    },
+                    DEFAULT: '#9290C9',
+                    secondary: '#7D7BB4',
+                },
+                pink: {
+                    bg: {
+                        DEFAULT: '#EACBDC',
+                    },
+                    DEFAULT: '#BA8BA6',
+                },
             },
             textColor: {
                 gray: '#BDBDBD',
                 lightGray: '#B2A3A1',
-                softPurple: '#9290C9',
-                darkPink: '#AB859B',
             }
         },
     },
